e563e1e5fd5228997d0af062f6b96d390f14fb1b
huong <huong@HUONG>
2024-10-27 23:13:43 +0700

    demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/header/header";
 import NavBar from "@/components/header/nav-bar";
+import Footer from "@/components/Footer/footer";
 
 export default function Home() {
   return (
     <div>
       {/* <NavBar></NavBar> */}
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
