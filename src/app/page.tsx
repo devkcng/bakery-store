1e056fe946046477df20c885a589956f44294f83
shannonnonshan <khanhminhdoan04@gmail.com>
2024-10-30 01:29:40 +0700

    OrderInfo-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Header from "@/components/header/header";
-import ItemCard from "@/components/item-card/item-card";
+import ItemCard, {ItemProps} from "@/components/item-card/item-card";
 import MenuTabs from "@/components/menu-tabs/menu-tabs";
 import SearchBar from "@/components/search-bar/search-bar";
 import MenuSection from "@/components/section/menu-section";
@@ -10,7 +10,8 @@ import { useState } from "react";
 import NavBar from "@/components/header/nav-bar";
 import Footer from "@/components/Footer/footer";
 import HomePage from "./home/page";
-
+import ItemOrder from "@/components/item-order/item-order";
+import OrderInfoSection from "@/components/section/orderInfo-section";
 export default function Home() {
   // const [selectedOption, setSelectedOption] = useState<string>("HTML");
   // const options: string[] = ["HTML", "React", "Vue", "Angular"];
@@ -19,12 +20,25 @@ export default function Home() {
   //   setSelectedOption(option);
   // };
   // console.log(selectedOption);
+  // const itemInfo: ItemProps = {
+  //   imagePath: '', // Đường dẫn đến ảnh
+  //   itemName: '', // Tên món
+  //   itemPrice: 28000 // Giá món
+  // };
+  // const count = 2;
   return (
     <div>
       {/* <SearchBar className="mt-[50px]" isShow={true}></SearchBar> */}
       {/* <SearchBar className="mt-[50px]" isShow={false}></SearchBar> */}
       {/* <NavBar></NavBar> */}
-      <HomePage></HomePage>
+      {/* <HomePage></HomePage> */}
+      {/* <ItemOrder 
+          itemInfo={itemInfo}    
+          itemCount={count}         
+          itemTopping="" 
+          itemTotal={itemInfo.itemPrice*count}
+      /> */}
+      <OrderInfoSection></OrderInfoSection>
     </div>
   );
 }
---
+++ src/components/item-order/item-order.tsx
@@ -0,0 +1,44 @@
+import React,{FC} from 'react';
+import './item-order.css';
+import ItemCard, {ItemProps} from '../item-card/item-card';
+import internal from 'stream';
+export interface DetailOrder 
+{
+    itemInfo: ItemProps;
+    itemCount: number;
+    itemTopping: string;
+}
+
+const ItemOrder: FC<DetailOrder> = ({itemInfo, itemCount, itemTopping}) => {
+    return (
+        <div className='container'>
+            <div className='pl-2.5'>
+                <img
+                src={`${
+                    itemInfo?.imagePath || "/imgs/bakery-images/muffinb.png"}`}
+                alt="" />
+            </div>
+            <div className='pl-2.5'>
+                <span className="block font-display text-[14px] font-semibold mt-1">
+                    {itemInfo?.itemName ? itemInfo.itemName : "Bánh Muffin"}
+                </span>{" "}
+                <span className="block font-display text-[14px] font-semibold text-[#797B7E] mt-2">
+                    {itemTopping === "" ? "nho khô, hạnh nhân" : itemTopping}
+                </span>
+                <span className="block font-display text-[11px] font-semibold text-[#797B7E] mt-4">
+                    {itemInfo?.itemPrice ===0 ? `Đơn giá: ${itemCount*itemInfo.itemPrice} VNĐ` : `Đơn giá: 28000 VNĐ`}    
+                </span>
+                <span className="block font-display text-[16px] font-semibold text-[#F67575] mb-2">
+                    {itemCount*itemInfo.itemPrice < 0 ? "Tổng: 28,000 VNĐ" : `Tổng: ${itemCount*itemInfo.itemPrice} VNĐ`}
+                </span>
+            </div>
+            <div className='pl-4'>
+            <span className="block text-center font-display text-[14px] font-semibold mt-4">
+                    {itemCount < 0 ? "0" : `SL: ${itemCount}`}
+                </span>
+            </div>
+        </div>
+    );
+};
+
+export default ItemOrder;
