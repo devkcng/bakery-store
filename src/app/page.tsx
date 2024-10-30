3cda41ac91c087251ac504c949bcd74e1c663709
zzVu77 <nguyenvanvu24062004@gmail.com>
2024-10-30 07:13:53 +0700

    "modify: updated Page.tsx, ItemOrder.tsx, OrderInfoSection.tsx, and orderInfo.css files with code refactoring and styling changes."

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Header from "@/components/header/header";
-import ItemCard, {ItemProps} from "@/components/item-card/item-card";
+import ItemCard, { ItemProps } from "@/components/item-card/item-card";
 import MenuTabs from "@/components/menu-tabs/menu-tabs";
 import SearchBar from "@/components/search-bar/search-bar";
 import MenuSection from "@/components/section/menu-section";
@@ -26,6 +26,10 @@ export default function Home() {
   //   itemPrice: 28000 // Giá món
   // };
   // const count = 2;
+  const time: string = new Date().toLocaleString("vi-VN", {
+    timeZone: "Asia/Ho_Chi_Minh",
+  });
+  console.log(time);
   return (
     <div>
       {/* <SearchBar className="mt-[50px]" isShow={true}></SearchBar> */}
@@ -38,7 +42,15 @@ export default function Home() {
           itemTopping="" 
           itemTotal={itemInfo.itemPrice*count}
       /> */}
-      <OrderInfoSection></OrderInfoSection>
+      <OrderInfoSection
+        idOrder="112233"
+        addressBuyer="So 1 Vo Van Ngan"
+        idStatus="Đã giao"
+        nameBuyer="Nguyễn Văn Vũ"
+        note="Giao cho bảo vệ"
+        phonenumber="0327521953"
+        time={time.toString()}
+      ></OrderInfoSection>
     </div>
   );
 }
--- src/components/section/orderInfo-section.tsx
+++ src/components/section/orderInfo-section.tsx
@@ -1,85 +1,106 @@
-import React, {FC} from 'react';
+import React, { FC } from "react";
 import "./orderInfo.css";
-import ItemOrder,{DetailOrder} from '../item-order/item-order';
-import ItemCard, {ItemProps} from "@/components/item-card/item-card";
-import SearchBar from '../search-bar/search-bar';
-const itemInfo: ItemProps = 
-{
-    imagePath: '', // Đường dẫn đến ảnh
-    itemName: '', // Tên món
-    itemPrice: 28000 // Giá món
+import ItemOrder, { DetailOrder } from "../item-order/item-order";
+import ItemCard, { ItemProps } from "@/components/item-card/item-card";
+import SearchBar from "../search-bar/search-bar";
+const itemInfo: ItemProps = {
+  imagePath: "", // Đường dẫn đến ảnh
+  itemName: "", // Tên món
+  itemPrice: 28000, // Giá món
 };
-const items: DetailOrder[] = 
-[
-    {itemInfo, itemCount:2, itemTopping:'chân trâu, dậu đỏ'},
-    {itemInfo, itemCount:3, itemTopping:'mè đen'},
-    {itemInfo, itemCount:4, itemTopping:'mè đen'},
-    {itemInfo, itemCount:5, itemTopping:'hạnh nhân'}
-]
-export interface orderInfo 
-{
-    idOrder: string;
-    idStatus:string;
-    time: string;
-    nameBuyer: string;
-    addressBuyer: string;
-    phonenumber: string;
-    note: string;
-};
-const OrderInfoSection:FC<orderInfo> = ({idOrder,idStatus,time,nameBuyer,addressBuyer, phonenumber,note}) => {
-    return (
-       <div id='container'>
-            <div className='flex flex-col items-center mt-10 mb-10 '>
-                <SearchBar isShow={true} className='searchBarLarger '/>
-                <div className='flex flex-row items-end'>
-                    <span className="block font-display text-[14px] font-semibold text-[#797B7E] mt-2">
-                        {idOrder ? `ID đơn hàng: ${idOrder}` : "ID đơn hàng: 1158041042024"}
-                    </span>
-                    <label className="block font-display text-[14px] font-semibold text-[#EA5848] mt-2 ml-5">
-                        {idStatus ? `| ${idOrder}` : "| Đang cập nhật"}
-                    </label>
-                </div>
-            </div>
-            <div className='flex flex-row justify-around mt-10 mb-20 h-auto'>
-                <div>
-                <span className="block text-center font-display text-[16px] font-bold mt-1 mb-5">
-                    Thông tin đơn hàng
-                </span>
+const items: DetailOrder[] = [
+  { itemInfo, itemCount: 2, itemTopping: "chân trâu, dậu đỏ" },
+  { itemInfo, itemCount: 3, itemTopping: "mè đen" },
+  { itemInfo, itemCount: 4, itemTopping: "mè đen" },
+  { itemInfo, itemCount: 5, itemTopping: "hạnh nhân" },
+];
+export interface orderInfo {
+  idOrder: string;
+  idStatus: string;
+  time: string;
+  nameBuyer: string;
+  addressBuyer: string;
+  phonenumber: string;
+  note: string;
+}
+const OrderInfoSection: FC<orderInfo> = ({
+  idOrder,
+  idStatus,
+  time,
+  nameBuyer,
+  addressBuyer,
+  phonenumber,
+  note,
+}) => {
+  return (
+    <div id="container">
+      <div className="flex flex-col items-center mt-10 mb-10 ">
+        <SearchBar isShow={true} className="searchBarLarger " />
+        <div className="flex flex-row items-end">
+          <span className="block font-display text-[14px] font-semibold text-[#797B7E] mt-2">
+            {idOrder ? `ID đơn hàng: ${idOrder}` : "ID đơn hàng: 1158041042024"}
+          </span>
+          <label className="block font-display text-[14px] font-semibold text-[#EA5848] mt-2 ml-5">
+            {idStatus ? `| ${idStatus}` : "| Đang cập nhật"}
+          </label>
+        </div>
+      </div>
+      <div className="flex flex-row justify-around m-auto h-auto">
+        <div>
+          <div className="flex flex-col">
+            <span className="block text-center font-display text-[16px] font-bold mt-1 mb-5">
+              Thông tin đơn hàng
+            </span>
+            <div className="list-order grid grid-rows-1 h-[450px] w-[500px] ">
+              <div className="m-auto w-auto ">
                 {items.map((item, index) => (
-                    <ItemOrder key={index} 
-                    itemInfo={item.itemInfo} 
-                    itemCount={item.itemCount}
-                    itemTopping={item.itemTopping}/>
-                    ))}
-                </div>
-                <div className="bg-black w-[5px] h-[full]"></div>
-                <div className='flex flex-col'>
-                <span className="block text-center font-display text-[16px] font-bold mt-1 mb-10">
-                    Thông tin người nhận
-                </span>
-                <span className="block font-display text-[16px] mt-1 mb-5">
-                    {idOrder ? `Đơn hàng: ${idOrder}` : "Đơn hàng: 1158041042024"}
-                </span>
-                <span className="block font-display text-[16px] mt-1 mb-5">
-                    {time ? `Đặt hàng lúc: ${time}` : "Đặt hàng lúc: 13:57 ngày 12/12"}
-                </span>
-                <span className="block font-display text-[16px] font-semibold mt-10 mb-5">
-                    {nameBuyer ? `Họ và tên người nhận: ${nameBuyer}` : "Họ và tên người nhận: Nguyễn Văn Vũ"}
-                </span>
-                <span className="block font-display text-[16px] font-semibold mt-1 mb-5">
-                    {addressBuyer ? `Địa chỉ người nhận: ${addressBuyer}` : "Địa chỉ người nhận: Số 1, Võ Văn Ngân, Tp. Thủ Đức"}
-                </span>
-                <span className="block font-display text-[16px] font-semibold mt-1 mb-5">
-                    {phonenumber ? `SĐT: ${phonenumber}` : "SĐT: 0327521953"}
-                </span>
-                <span className="block font-display text-[16px] font-semibold mt-1 mb-5">
-                    {note ? `Ghi chú: ${note}` : "Ghi chú: Để hàng ở gốc cây"}
-                </span>
-                </div>
+                  <div key={index} className="mb-2">
+                    <ItemOrder
+                      itemInfo={item.itemInfo}
+                      itemCount={item.itemCount}
+                      itemTopping={item.itemTopping}
+                    />
+                  </div>
+                ))}
+              </div>
             </div>
-       </div>
-       
-    );
+          </div>
+          <div className="font-semibold text-red-600 text-[20px] h-[10px] text-center mt-2">
+            Tổng tiền: 200000 VNĐ
+          </div>
+        </div>
+
+        <div className="bg-black w-[5px] h-[full] text-center"></div>
+        <div className="flex flex-col">
+          <span className="block text-center font-display text-[16px] font-bold mt-1 mb-10">
+            Thông tin người nhận
+          </span>
+          <span className="block font-display text-[16px] mt-1 mb-5">
+            {idOrder ? `Đơn hàng: ${idOrder}` : "Đơn hàng: 1158041042024"}
+          </span>
+          <span className="block font-display text-[16px] mt-1 mb-5">
+            {time ? `Đặt hàng lúc: ${time}` : "Đặt hàng lúc: 13:57 ngày 12/12"}
+          </span>
+          <span className="block font-display text-[16px] font-semibold mt-10 mb-5">
+            {nameBuyer
+              ? `Họ và tên người nhận: ${nameBuyer}`
+              : "Họ và tên người nhận: Nguyễn Văn Vũ"}
+          </span>
+          <span className="block font-display text-[16px] font-semibold mt-1 mb-5">
+            {addressBuyer
+              ? `Địa chỉ người nhận: ${addressBuyer}`
+              : "Địa chỉ người nhận: Số 1, Võ Văn Ngân, Tp. Thủ Đức"}
+          </span>
+          <span className="block font-display text-[16px] font-semibold mt-1 mb-5">
+            {phonenumber ? `SĐT: ${phonenumber}` : "SĐT: 0327521953"}
+          </span>
+          <span className="block font-display text-[16px] font-semibold mt-1 mb-5">
+            {note ? `Ghi chú: ${note}` : "Ghi chú: Để hàng ở gốc cây"}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OrderInfoSection;
